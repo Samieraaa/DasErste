fb097c92649b2d28c9abfff56a8307a464555dd8
Samieraaa <keshavarz.samira1989@gmail.com>
2023-01-03 20:07:40 +0330

    test

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import App from './App'
+import ReactDOM from 'react-dom'
+
+// const el = <h1>Hello World</h1>
+
+// const element = <ul>
+//     <li>1</li>
+//     <li>2</li>
+//     <li>3</li>
+
+
+// </ul>
+
+// const element = React.createElement('h2', {}, 'You can change the text!')
+
+ReactDOM.render(<App />, document.getElementById('root'))
